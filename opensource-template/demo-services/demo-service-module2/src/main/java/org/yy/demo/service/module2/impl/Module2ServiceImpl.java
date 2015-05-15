8a6ded82cf8c5d898ca664ea89cb377cdfc07021
zl2006 <zl2006@gmail.com>
2015-05-15 11:22:20 +0800

    使用注解取代xml配置

--- opensource-template/demo-services/demo-service-module2/src/main/java/org/yy/demo/service/module2/impl/Module2ServiceImpl.java
+++ opensource-template/demo-services/demo-service-module2/src/main/java/org/yy/demo/service/module2/impl/Module2ServiceImpl.java
@@ -1,7 +1,9 @@
 package org.yy.demo.service.module2.impl;
 
+import org.springframework.stereotype.Service;
 import org.yy.demo.service.module2.Module2Service;
 
+@Service("module2Service")
 public class Module2ServiceImpl implements Module2Service {
 
 	@Override
